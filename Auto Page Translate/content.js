8ed87940098f22eb8aba0abe7bb543f1c9ba069f
har <99115857+86har@users.noreply.github.com>
2023-10-08 00:49:16 +0900

    Update content.js

--- Auto Page Translate/content.js
+++ Auto Page Translate/content.js
@@ -6,23 +6,16 @@ function getIFLAME(){
       if (video) {
           // videoが終了したらconsole.logで出力
           video.addEventListener('ended', function() {
+            for(let i = 0; i<5000; i+=1000){
               setTimeout(() => {
                 document.getElementsByClassName("has-no-padding")[0].style.backgroundColor = "red";
                 iframeElement.contentWindow.document.getElementsByClassName("feLoHN")[0].style.backgroundColor = "red";
-                setTimeout(() => {
-                  document.getElementsByClassName("has-no-padding")[0].style.backgroundColor = "white";
-                  iframeElement.contentWindow.document.getElementsByClassName("feLoHN")[0].style.backgroundColor = "white";
-                  setTimeout(() => {
-                    document.getElementsByClassName("has-no-padding")[0].style.backgroundColor = "red";
-                    iframeElement.contentWindow.document.getElementsByClassName("feLoHN")[0].style.backgroundColor = "red";
-                    setTimeout(() => {
-                      document.getElementsByClassName("has-no-padding")[0].style.backgroundColor = "white";
-                      iframeElement.contentWindow.document.getElementsByClassName("feLoHN")[0].style.backgroundColor = "white";
-                    }, 500);
-                  }, 500);
-                }, 500);
-              }, 100);
-              
+              }, 0+i);
+              setTimeout(() => {
+                document.getElementsByClassName("has-no-padding")[0].style.backgroundColor = "white";
+                iframeElement.contentWindow.document.getElementsByClassName("feLoHN")[0].style.backgroundColor = "white";
+              }, 500+i);
+            }
           });
       }
       setTimeout(getIFLAME, 1000);
